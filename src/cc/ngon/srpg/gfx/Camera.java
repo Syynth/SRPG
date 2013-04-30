5d0a09416a1f23852d8c7b9a5bfd04f95034cbea
Benjamin Cochrane <ben@ngon.cc>
2013-04-30 11:11:01 -0400

    Added Slick Utils, and a Camera class.

---
+++ src/cc/ngon/srpg/gfx/Camera.java
@@ -0,0 +1,70 @@
+/**
+ * @date Apr 30, 2013
+ * @author Ben Cochrane
+ */
+package cc.ngon.srpg.gfx;
+
+import org.lwjgl.util.vector.Vector3f;
+import static org.lwjgl.opengl.GL11.*;
+
+public class Camera {
+    
+    public Camera() {
+        this(60, 640/480, 0.1f, 1000);
+    }
+
+    public Camera(float angle, float aspect, float znear, float zfar) {
+        this.angle = angle;
+        this.aspect = aspect;
+        this.znear = znear;
+        this.zfar = zfar;
+
+        position = new Vector3f();
+        rotation = new Vector3f();
+    }
+
+    public Camera moveTo(Vector3f position) {
+        this.position.x = position.x;
+        this.position.y = position.y;
+        this.position.z = position.z;
+        return this;
+    }
+
+    public Camera moveBy(Vector3f delta) {
+        this.position.x += delta.x;
+        this.position.y += delta.y;
+        this.position.z += delta.z;
+        return this;
+    }
+
+    public Camera rotateTo(Vector3f rotation) {
+        this.rotation.x = rotation.x;
+        this.rotation.y = rotation.y;
+        this.rotation.z = rotation.z;
+        return this;
+    }
+
+    public Camera rotateBy(Vector3f delta) {
+        this.rotation.x += delta.x;
+        this.rotation.y += delta.y;
+        this.rotation.z += delta.z;
+        return this;
+    }
+    
+    public Camera apply() {
+        glLoadIdentity();
+        glRotatef(rotation.x, 1, 0, 0);
+        glRotatef(rotation.y, 0, 1, 0);
+        glRotatef(rotation.z, 0, 0, 1);
+        glTranslatef(position.x, position.y, position.z);
+        return this;
+    }
+    
+    public float angle;
+    public float aspect;
+    public float znear;
+    public float zfar;
+    public Vector3f position;
+    public Vector3f rotation;
+    
+}
